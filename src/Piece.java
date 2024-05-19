222ca0a1aabbd936f17eae383f43c6a490915d7f
dragon3141 <drakonmaster314@gmail.com>
2024-05-18 17:45:32 -0700

    Piece abstract class base

--- src/Piece.java
+++ src/Piece.java
@@ -1,5 +1,9 @@
-//nya
-public class Piece {
-
-
+public abstract class Piece {
+	protected boolean color;
+	
+	Piece(boolean color){
+		this.color = color;
+	}
+	
+	public abstract boolean canMove(int curX, int curY, int x, int y);
 }
